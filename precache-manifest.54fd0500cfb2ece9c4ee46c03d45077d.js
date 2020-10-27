bf2949e146dc42f11c193bdd05f93201d7c5f37a
Khalil <asif_khalil5@optum.com>
2020-10-28 00:49:44 +0530

    Updates

---
+++ precache-manifest.54fd0500cfb2ece9c4ee46c03d45077d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9306c5ee52ec80c6d71edb02b56bbb6c",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "b3fdc71752070636aa42",
+    "url": "/reactapp/static/css/main.1be22f0d.chunk.css"
+  },
+  {
+    "revision": "97f0fe28d2e2c7e44f9e",
+    "url": "/reactapp/static/js/2.8e855b46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.8e855b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fdc71752070636aa42",
+    "url": "/reactapp/static/js/main.fe84e00b.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  }
+]);
